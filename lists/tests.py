fbc8d7b45131a4142fa6663bcbd8fc6f1bd3d306
Jonathan Zahedieh <zahesibw@gmail.com>
2014-03-31 00:15:03 +0100

    New list template and updated homepage

--- lists/tests.py
+++ lists/tests.py
@@ -44,16 +44,7 @@ class HomePageTest(TestCase):
 		request = HttpRequest()
 		home_page(request)
 		self.assertEqual(Item.objects.count(), 0)
-		
-	def test_home_page_displays_all_list_items(self):
-		Item.objects.create(text='item 1')
-		Item.objects.create(text='item 2')
-		
-		request = HttpRequest()
-		response = home_page(request)
-		
-		self.assertIn('item 1', response.content.decode())
-		self.assertIn('item 2', response.content.decode())
+
 		
 class ItemModelTest(TestCase):
 
@@ -84,3 +75,7 @@ class ListViewTest(TestCase):
 		
 		self.assertContains(response, 'item 1')
 		self.assertContains(response, 'item 2')
+		
+	def test_uses_list_template(self):
+		response = self.client.get('/lists/the-only-list-in-the-world/')
+		self.assertTemplateUsed(response, 'list.html')
