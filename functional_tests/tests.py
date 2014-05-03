23f2591617ce190db18e5be461cd2ce2fca6a155
Jonathan Zahedieh <zahesibw@gmail.com>
2014-05-03 10:50:37 +0100

    FT user story for empty list

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -3,6 +3,7 @@ import sys
 from django.contrib.staticfiles.testing import StaticLiveServerCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from unittest import skip
 
 class NewVisitorTest(StaticLiveServerCase):
 
@@ -121,4 +122,22 @@ class NewVisitorTest(StaticLiveServerCase):
 			512,
 			delta=10
 		)
+
+	@skip
+	def test_cannot_add_empty_list_items(self):
+		# Edith goes the the homepage and accidentally tries to submit
+		# a empty list item, she hits enter on the empty input box
+
+		# The homepage refreshes and there is an error message saying that
+		# the list items cannot be blank
+
+		# she tries again with some text for the item, which now works
+
+		# she now decides to submit a second blnk list item
+
+		# she receives  a similar warning onthe list page
+
+		# and she can correct it by filling some text in
+
+		self.fail('write me')
 	
