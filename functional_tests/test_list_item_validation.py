a31776b87ae9688e8b9cae6c275d96a854cf3805
Jonathan Zahedieh <zahesibw@gmail.com>
2014-05-05 15:32:19 +0100

    Duplcate items FTs

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -1,4 +1,5 @@
 from .base import FunctionalTest
+from lists.forms import EMPTY_LIST_ERROR, DUPLICATE_ITEM_ERROR
 
 class ItemValidationTest(FunctionalTest):
 
@@ -11,7 +12,7 @@ class ItemValidationTest(FunctionalTest):
 		# The homepage refreshes and there is an error message saying that
 		# the list items cannot be blank
 		error = self.browser.find_element_by_css_selector('.has-error')
-		self.assertEqual(error.text, "You can't have an empty list item")
+		self.assertEqual(error.text, EMPTY_LIST_ERROR)
 
 		# she tries again with some text for the item, which now works
 		self.get_item_input_box().send_keys('Buy milk\n')
@@ -23,11 +24,24 @@ class ItemValidationTest(FunctionalTest):
 		# she receives  a similar warning on the list page
 		self.check_for_row_in_list_table('1: Buy milk')
 		error = self.browser.find_element_by_css_selector('.has-error')
-		self.assertEqual(error.text, "You can't have an empty list item")
+		self.assertEqual(error.text, EMPTY_LIST_ERROR)
 
 		# and she can correct it by filling some text in
 		self.get_item_input_box().send_keys('Make tea\n')
 		self.check_for_row_in_list_table('1: Buy milk')
 		self.check_for_row_in_list_table('2: Make tea')
-
+		
+	def test_cannot_add_duplicate_items(self):
+		# Edith goes to the home page and starts a new list
+		self.browser.get(self.server_url)
+		self.get_item_input_box().send_keys('Buy wellies\n')
+		self.check_for_row_in_list_table('1: Buy wellies')
+		
+		# She accidentally tries to enter a duplicate item
+		self.get_item_input_box().send_keys('Buy wellies\n')
+		
+		# She sees a helpful error message
+		self.check_for_row_in_list_table('1: Buy wellies')
+		error = self.browser.find_element_by_css_selector('.has-error')
+		self.assertEqual(error.text, DUPLICATE_ITEM_ERROR)
 	
